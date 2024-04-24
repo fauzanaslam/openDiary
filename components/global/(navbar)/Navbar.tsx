186db25cdc198e28f4b2ae032ca190b257da959e
fauzanaslam <fauzanaslamdhani@gmail.com>
2024-04-24 14:49:59 +0700

    navbar

--- components/global/(navbar)/Navbar.tsx
+++ components/global/(navbar)/Navbar.tsx
@@ -5,7 +5,7 @@ import NavbarButton from "./NavbarButton";
 const Navbar = (): React.ReactElement => {
   return (
     <div className="navbar bg-base-100">
-      <div className="container flex mx-auto justify-between p-4">
+      <div className="container flex mx-auto justify-between p-4 gap-5">
         <Link href="/" className="text-xl font-bold">
           OpenDiary
         </Link>
