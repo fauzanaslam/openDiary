a094f698f3136ce6c84c545a2c9f69bbe07914c5
fauzanaslam <fauzanaslamdhani@gmail.com>
2024-04-27 13:15:41 +0700

    spp

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,9 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-import { UUID } from "crypto";
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-  process.env.NEXT_PUBLIC_SUPABASE_SECRET_KEY as string
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 );
 
 export interface IDiary {
