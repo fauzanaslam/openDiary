a24989582ab5cc5c920da43f31b5cac18e69ba99
fauzanaslam <fauzanaslamdhani@gmail.com>
2024-05-17 18:56:46 +0700

    aa

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
               FauzanAslam
             </p>
           </Link>
-          X mang Dea & OpenDiary Dev Team. All rights reversed
+           X mang Dea & OpenDiary Dev Team. All rights reversed
           <p>.</p>
         </div>
       </div>
