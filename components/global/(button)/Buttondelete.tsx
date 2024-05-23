9e290e72702359d4d07d749e60543f1988e38138
fauzanaslam <fauzanaslamdhani@gmail.com>
2024-05-23 09:33:41 +0700

    feat: add modal before delete diary

--- components/global/(button)/Buttondelete.tsx
+++ components/global/(button)/Buttondelete.tsx
@@ -1,34 +1,56 @@
 "use client";
 
 import deleteDiaryAction from "@/actions/deleteDiaryAction";
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineDelete } from "react-icons/ai";
 
 type ParamsProps = {
   diary_id: number | undefined;
 };
 
 const Buttondelete = ({ diary_id }: ParamsProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleDelete = async () => {
+    setIsLoading(true);
+    try {
+      await deleteDiaryAction(diary_id);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div>
       <button
-        onClick={() => deleteDiaryAction(diary_id)}
         className="btn btn-square btn-outline btn-warning"
+        onClick={handleModal}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
+        <AiOutlineDelete size={28} />
       </button>
+      <div className={isOpen ? "modal modal-open" : "modal"}>
+        <div className="modal-box">
+          <h3>are you sure to delete this diary?</h3>
+          <div className="modal-action">
+            <button className="btn" onClick={handleModal}>
+              No
+            </button>
+            <button
+              className={isLoading ? "btn loading" : "btn btn-warning"}
+              onClick={handleDelete}
+            >
+              Yes
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -1,21 +1,9 @@
 import React from "react";
-import Link from "next/link";
 
 const Footer = () => {
   return (
     <div className="flex justify-center py-5 bg-slate-800 mt-12">
-      <div className="md:flex gap-1">
-        <p className="text-center">@ OpenDiary 2024 |</p>
-        <div className="flex">
-          <Link href="https://www.instagram.com/fauzanaslam/?next=%2F">
-            <p className="hover:text-primary ease-in-out duration-100">
-              FauzanAslam
-            </p>
-          </Link>
-           X mang Dea & OpenDiary Dev Team. All rights reversed
-          <p>.</p>
-        </div>
-      </div>
+      <p className="text-center">@ OpenDiary 2024 | All rights reversed.</p>
     </div>
   );
 };
