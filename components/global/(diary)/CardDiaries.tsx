c24ab62aff8ab56a540f57bbaf9838dc0aee9f3d
fauzanaslam <fauzanaslamdhani@gmail.com>
2024-05-27 12:26:16 +0700

    feat: skeleton but deleting like function

--- components/global/(diary)/CardDiaries.tsx
+++ components/global/(diary)/CardDiaries.tsx
@@ -1,49 +1,80 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import PostContent from "./PostContent";
 import { supabase } from "@/utils/supabase";
 import LikesButton from "../(button)/LikesButton";
-import { getUserData } from "@/utils/clerk";
 import ShareButton from "../(button)/ShareButton";
 import ScreenshootButton from "../(button)/ScreenshootButton";
 import { CgComment } from "react-icons/cg";
 import Link from "next/link";
+import { getAuth } from "@clerk/nextjs/server";
+import Skeleton from "../Skeleton";
 
-const CardDiaries = async (): Promise<React.ReactElement> => {
-  const { email } = await getUserData();
-  const { data, error } = await supabase
-    .from("diary")
-    .select()
-    .order("created_at", { ascending: false });
+const CardDiaries = (): React.ReactElement => {
+  const [diaries, setDiaries] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  // const [email, setEmail] = useState<string>("");
 
-  if (error) return <p>please reload the page</p>;
+  useEffect(() => {
+    const fetchDiaries = async () => {
+      try {
+        // const userData = await getUserData();
+        // if (userData.email) {
+        //   setEmail(userData.email);
+        // }
+        const { data, error } = await supabase
+          .from("diary")
+          .select()
+          .order("created_at", { ascending: false });
+        console.log("🚀 ~ fetchDiaries ~ data:", data);
+
+        if (error) throw error;
+        setDiaries(data || []);
+      } catch (error) {
+        console.error("Error fetching diaries:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDiaries();
+  }, []);
+
+  if (loading) {
+    return <Skeleton />;
+  }
+
+  if (diaries.length === 0) {
+    return <p>No diaries found</p>;
+  }
+
   return (
     <div>
-      {data.map((diary) => {
-        return (
-          <div key={diary.id} className="relative border-b-2 border-gray-700">
-            <div id={diary.id}>
-              <PostContent
-                key={diary.id}
-                diary_id={diary.id}
-                avatar={diary.avatar}
-                content={diary.content}
-                email={diary.email}
-                username={diary.username}
-                diary_image={diary.diary_image}
-                created_at={diary.created_at}
-              />
-            </div>
-            <div className="flex items-center absolute ml-20 mb-3 -bottom-2 gap-2">
-              <LikesButton email={email} diary={diary} />
-              <ShareButton diary_id={diary.id} />
-              <Link href={`/diary/${diary.id}`}>
-                <CgComment />
-              </Link>
-              <ScreenshootButton elementId={diary.id} />
-            </div>
+      {diaries.map((diary) => (
+        <div key={diary.id} className="relative border-b-2 border-gray-700">
+          <div id={diary.id}>
+            <PostContent
+              key={diary.id}
+              diary_id={diary.id}
+              avatar={diary.avatar}
+              content={diary.content}
+              email={diary.email}
+              username={diary.username}
+              diary_image={diary.diary_image}
+              created_at={diary.created_at}
+            />
           </div>
-        );
-      })}
+          <div className="flex items-center absolute ml-20 mb-3 -bottom-2 gap-2">
+            {/* <LikesButton email={email} diary={diary} /> */}
+            <ShareButton diary_id={diary.id} />
+            <Link href={`/diary/${diary.id}`}>
+              <CgComment />
+            </Link>
+            <ScreenshootButton elementId={diary.id} />
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
---
+++ components/global/Skeleton.tsx
@@ -0,0 +1,59 @@
+const Skeleton = () => {
+  return (
+    <div className="animate-pulse">
+      <div className="flex flex-1 gap-2 px-5 pb-5 pt-3">
+        <div className="rounded-full bg-gray-800 w-11 h-11" />
+        <div className="flex flex-col gap-2">
+          <div className="flex gap-2">
+            <div className="flex gap-2 items-center rounded-full bg-gray-800 w-32 h-5"></div>
+            <div className="flex gap-2 items-center rounded-full bg-gray-800 w-24 h-5"></div>
+          </div>
+          <div className="flex gap-2 items-center rounded-full bg-gray-800 w-50 md:w-96 h-5"></div>
+          <div className="flex gap-2 items-center rounded-full bg-gray-800 w-50 md:w-96 h-5"></div>
+          <div className="flex gap-2 items-center rounded-full bg-gray-800 w-50 md:w-96 h-5"></div>
+        </div>
+      </div>
+      <hr />
+      <div className="flex flex-1 gap-2 px-5 pb-5 pt-3">
+        <div className="rounded-full bg-gray-800 w-11 h-11" />
+        <div className="flex flex-col gap-2">
+          <div className="flex gap-2">
+            <div className="flex gap-2 items-center rounded-full bg-gray-800 w-32 h-5"></div>
+            <div className="flex gap-2 items-center rounded-full bg-gray-800 w-24 h-5"></div>
+          </div>
+          <div className="flex gap-2 items-center rounded-full bg-gray-800 w-50 md:w-96 h-5"></div>
+          <div className="flex gap-2 items-center rounded-full bg-gray-800 w-50 md:w-96 h-5"></div>
+          <div className="flex gap-2 items-center rounded-full bg-gray-800 w-50 md:w-96 h-5"></div>
+        </div>
+      </div>
+      <hr />
+      <div className="flex flex-1 gap-2 px-5 pb-5 pt-3">
+        <div className="rounded-full bg-gray-800 w-11 h-11" />
+        <div className="flex flex-col gap-2">
+          <div className="flex gap-2">
+            <div className="flex gap-2 items-center rounded-full bg-gray-800 w-32 h-5"></div>
+            <div className="flex gap-2 items-center rounded-full bg-gray-800 w-24 h-5"></div>
+          </div>
+          <div className="flex gap-2 items-center rounded-full bg-gray-800 w-50 md:w-96 h-5"></div>
+          <div className="flex gap-2 items-center rounded-full bg-gray-800 w-50 md:w-96 h-5"></div>
+          <div className="flex gap-2 items-center rounded-full bg-gray-800 w-50 md:w-96 h-5"></div>
+        </div>
+      </div>
+      <hr />
+      <div className="flex flex-1 gap-2 px-5 pb-5 pt-3">
+        <div className="rounded-full bg-gray-800 w-11 h-11" />
+        <div className="flex flex-col gap-2">
+          <div className="flex gap-2">
+            <div className="flex gap-2 items-center rounded-full bg-gray-800 w-32 h-5"></div>
+            <div className="flex gap-2 items-center rounded-full bg-gray-800 w-24 h-5"></div>
+          </div>
+          <div className="flex gap-2 items-center rounded-full bg-gray-800 w-50 md:w-96 h-5"></div>
+          <div className="flex gap-2 items-center rounded-full bg-gray-800 w-50 md:w-96 h-5"></div>
+          <div className="flex gap-2 items-center rounded-full bg-gray-800 w-50 md:w-96 h-5"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skeleton;
